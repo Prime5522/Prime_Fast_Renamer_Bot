4af9cceaa64bb1904e02e1a55ddd9ed6d0d31cb7
PRIME BOT'z <sahidhasannayem@gmail.com>
2024-10-23 05:15:51 +0600

    Update upgrade.py

--- plugins/upgrade.py
+++ plugins/upgrade.py
@@ -5,20 +5,20 @@ from pyrogram import Client , filters
 @Client.on_callback_query(filters.regex('upgrade'))
 async def upgrade(bot,update):
 	text = """**Free Plan User**
-	Daily  Upload limit 1.2GB
+	Daily  Upload limit 3GB
 	Price 0
 	
 	**🪙 Silver Tier 🪙** 
 	Daily  Upload  limit 10GB
-	Price Rs 66  ind /🌎 0.8$  per Month
+	Price 50 ₹ /🌎 0.8$  per Month
 	
 	**💫 Gold Tier 💫**
 	Daily Upload limit 50GB
-	Price Rs 100  ind /🌎 1.2$  per Month
+	Price 100 ₹ /🌎 1.2$  per Month
 	
 	**💎 Diamond 💎**
 	Daily Upload limit 100GB
-	Price Rs 206  ind /🌎 2.5$  per Month
+	Price 199 ₹ /🌎 2.5$  per Month
 	
 	
 	Pay Using Upi I'd ```7808912076@paytm```
@@ -35,20 +35,20 @@ async def upgrade(bot,update):
 @Client.on_message(filters.private & filters.command(["upgrade"]))
 async def upgradecm(bot,message):
 	text = """**Free Plan User**
-	Daily  Upload limit 1.2GB
+	Daily  Upload limit 3GB
 	Price 0
 	
 	**🪙 Silver Tier 🪙** 
 	Daily  Upload  limit 10GB
-	Price Rs 66  ind /🌎 0.8$  per Month
+	Price 50 ₹ /🌎 0.8$  per Month
 	
 	**💫 Gold Tier 💫**
 	Daily Upload limit 50GB
-	Price Rs 100  ind /🌎 1.2$  per Month
+	Price 100 ₹ /🌎 1.2$  per Month
 	
 	**💎 Diamond 💎**
 	Daily Upload limit 100GB
-	Price Rs 206  ind /🌎 2.5$  per Month
+	Price 199 ₹ /🌎 2.5$  per Month
 	
 	
 	Pay Using Upi I'd ```7808912076@paytm```
@@ -56,7 +56,7 @@ async def upgradecm(bot,message):
 	After Payment Send Screenshots Of 
         Payment To Admin @mRiderDM"""
 	keybord = InlineKeyboardMarkup([[ 
-        			InlineKeyboardButton("ADMIN 🛂",url = "https://t.me/mRiDerDM")], 
+        			InlineKeyboardButton("ADMIN SUPPORT 🛂",url = "https://t.me/mRiDerDM")], 
         			[InlineKeyboardButton("Paytm",url = "https://p.paytm.me/xCTH/vo37hii9"),
         			InlineKeyboardButton("Paytm",url = "https://p.paytm.me/xCTH/vo37hii9")],[InlineKeyboardButton("Cancel",callback_data = "cancel")  ]])
 	await message.reply_text(text = text,reply_markup = keybord)
