c3a76065a19ef221859f55691934465c98ce3ef8
PRIME BOT'z <sahidhasannayem@gmail.com>
2024-10-23 05:52:34 +0600

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -17,7 +17,7 @@ import os
 
 CHANNEL = os.environ.get('CHANNEL', "")
 STRING = os.environ.get("STRING", "")
-ADMIN = int(os.environ.get("ADMIN", 1484670284))
+ADMIN = int(os.environ.get("ADMIN", ))
 bot_username = os.environ.get("BOT_USERNAME","")
 log_channel = int(os.environ.get("LOG_CHANNEL", ""))
 token = os.environ.get('TOKEN', '')
@@ -64,11 +64,11 @@ async def start(client, message):
                 await message.reply_photo(photo=LAZY_PIC,
                                          caption=txt,
                                          reply_markup=InlineKeyboardMarkup(
-                                             [[InlineKeyboardButton("🔺 Update Channel 🔺", url="https://t.me/LazyDeveloper")],
-                                              [InlineKeyboardButton("🦋 Subscribe us 🦋", url="https://youtube.com/@LazyDeveloperr")],
-                                              [InlineKeyboardButton("Support Group", url='https://t.me/LazyPrincessSupport'),
-                                             InlineKeyboardButton("Movie Channel", url='https://t.me/real_MoviesAdda2')],
-                                             [InlineKeyboardButton("☕ Buy Me A Coffee ☕", url='https://p.paytm.me/xCTH/vo37hii9')]
+                                             [[InlineKeyboardButton("🔺 Update Channel 🔺", url="https://t.me/PRIME_BOTZ")],
+                                              [InlineKeyboardButton("🦋 Admin Support 🦋", url="https://t.me/Prime_Bots_Support_RoBot")],
+                                              [InlineKeyboardButton("🔎Movie Request Group🔍", url='https://t.me/Prime_Movies4U/143'),
+                                             InlineKeyboardButton("✨ Movie Channel ✨", url='https://t.me/Prime_Movies4U')],
+                                             [InlineKeyboardButton("☕ Buy Me A Coffee ☕", url='https://envs.sh/0lv.jpg')]
                                           ]))
             except:
                 return
@@ -84,11 +84,11 @@ async def start(client, message):
 	**Document Or Video** and enter new filename to rename it__
 	""", reply_to_message_id=message.id,
                                      reply_markup=InlineKeyboardMarkup(
-                                         [[InlineKeyboardButton("🔺 Update Channel 🔺", url="https://t.me/LazyDeveloper")],
-                                          [InlineKeyboardButton("🦋 Subscribe us 🦋", url="https://youtube.com/@LazyDeveloperr")],
-                                          [InlineKeyboardButton("Support Group", url='https://t.me/LazyPrincessSupport'),
-                                          InlineKeyboardButton("Movie Channel", url='https://t.me/real_MoviesAdda2')],
-                                          [InlineKeyboardButton("☕ Buy Me A Coffee ☕", url='https://p.paytm.me/xCTH/vo37hii9')]
+                                         [[InlineKeyboardButton("🔺 Update Channel 🔺", url="https://t.me/PRIME_BOTZ")],
+                                          [InlineKeyboardButton("🦋 Admin Support 🦋", url="https://t.me/Prime_Bots_Support_RoBot")],
+                                          [InlineKeyboardButton("🔎Movie Request Group🔍", url='https://t.me/Prime_Movies4U/143'),
+                                          InlineKeyboardButton("✨ Movie Channel ✨", url='https://t.me/Prime_Movies4U')],
+                                          [InlineKeyboardButton("☕ Buy Me A Coffee ☕", url='https://envs.sh/0lv.jpg')]
                                           ]))
     
 
@@ -126,11 +126,11 @@ async def send_doc(client, message):
     except:
         await message.reply_text(text=f"Hello dear {message.from_user.first_name}  **we are currently working on this issue**\n\nPlease try to rename files from your another account.\nBecause this BOT can't rename file sent by some ids.\n\nIf you are an **ADMIN** Don't worry ! here we have a solution for you dear {message.from_user.first_name }.\n\nPlease use \n👉 `/addpremium your_other_userid` 👈 to use premium feautres\n\n",
                                   reply_markup=InlineKeyboardMarkup([
-                                                                     [InlineKeyboardButton("🦋 Contact LazyDeveloper 🦋", url='https://telegram.me/LazyDeveloper')],
-                                                                     [InlineKeyboardButton("🔺 Watch Tutorial 🔺", url='https://youtube.com/@LazyDeveloperr')],
-                                                                     [InlineKeyboardButton("🦋 Visit Channel  ", url='https://t.me/LazyDeveloper'),
-                                                                     InlineKeyboardButton("  Support Group 🦋", url='https://t.me/LazyPrincessSupport')],
-                                                                     [InlineKeyboardButton("☕ Buy Me A Coffee ☕", url='https://p.paytm.me/xCTH/vo37hii9')]
+                                                                     [InlineKeyboardButton("🦋 Contact PrimeDeveloper 🦋", url='https://telegram.me/Prime_Nayem')],
+                                                                     [InlineKeyboardButton("🔺 Admin Support 🔺", url='https://t.me/Prime_Bots_Support_RoBot')],
+                                                                     [InlineKeyboardButton("🔥 Visit Channel 🔥", url='https://t.me/PRIME_BOTZ'),
+                                                                     InlineKeyboardButton("✨ Visit Movie Channel ✨", url='https://t.me/Prime_Movies4U')],
+                                                                     [InlineKeyboardButton("☕ Buy Me A Coffee ☕", url='https://envs.sh/0lv.jpg')]
                                                                     ]))
         await message.reply_text(text=f"🦋")
         return 
